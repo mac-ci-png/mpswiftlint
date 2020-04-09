4275de0115a926e64893b83968c40225dac9d13c
Peter Jenkins <pjenkins@mparticle.com>
2020-04-09 15:50:39 -0400

    Update version to 1.0.1

--- package.sh
+++ package.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 MPSWIFTLINT_TEMPORARY_FOLDER=/tmp/mpswiftlint/MPSwiftLintAdapter.app
-VERSION_STRING=1.0.0
+VERSION_STRING=1.0.1
 OUTPUT_PACKAGE=./MPSwiftLint.pkg
 DISTRIBUTION_PLIST=./mpswiftlint/Sources/Distribution.plist
 cp "/tmp/mpswiftlint/mpswiftlint" "$MPSWIFTLINT_TEMPORARY_FOLDER"
